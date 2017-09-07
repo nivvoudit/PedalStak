990ac7c7c0eae0cef933edf5180d887066d4fa67
Nivv Oudit <nivvoudit@gmail.com>
2017-09-07 16:36:13 -0500

    Added pedalboard

--- app/routes.js
+++ app/routes.js
@@ -20,6 +20,13 @@ module.exports = function(app, passport) {
 
     // process the login form
     // app.post('/login', do all our passport stuff here);
+    // process the login form
+    app.post('/login', passport.authenticate('local-login', {
+      successRedirect : '/profile', // redirect to the secure profile section
+      failureRedirect : '/login', // redirect back to the signup page if there is an error
+      failureFlash : true // allow flash messages
+    }));
+
 
     // =====================================
     // SIGNUP ==============================
@@ -32,12 +39,12 @@ module.exports = function(app, passport) {
     });
 
     // process the signup form
+       app.post('/signup', passport.authenticate('local-signup', {
+           successRedirect : '/profile', // redirect to the secure profile section
+           failureRedirect : '/signup', // redirect back to the signup page if there is an error
+           failureFlash : true // allow flash messages
+       }));
 
-    app.post('/signup', passport.authenticate('local-signup', {
-     successRedirect : '/profile', // redirect to the secure profile section
-     failureRedirect : '/signup', // redirect back to the signup page if there is an error
-     failureFlash : true // allow flash messages
-      }));
     // app.post('/signup', do all our passport stuff here);
 
     // =====================================
--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'your-settings-here' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://<dbuser>:<dbpassword>@ds129004.mlab.com:29004/heroku_2cl602hn' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
